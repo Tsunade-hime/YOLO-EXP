15be824469cb21b33eef30b608e410bff307321b
Tsunade-hime <HinNyuuHime@outlook.com>
2026-05-01 13:01:46 +0800

    Add C2f_MSEF to convolution module sets

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1675,10 +1675,10 @@ def parse_model(d, ch, verbose=True):
     conv_modules = frozenset({SPDConv, LAE, MSBlock, FasterBlock, RFEM, FCM,PConv,
                               CAB, MSCAM, SCConv, C3GhostExp, C3XExp, C3TRExp,
                               HGBlockExp, C3SCConvExp, C3FasterBlockExp, C2fMSBlockExp, C2fHGBlockExp,
-                              RFAConv, RFCAConv, RFCBAMConv, Sonic, C3_Sonic, C3_HoGEdgeGate, C2f_GLFA, C2f_PFG})
+                              RFAConv, RFCAConv, RFCBAMConv, Sonic, C3_Sonic, C3_HoGEdgeGate, C2f_GLFA, C2f_PFG, C2f_MSEF})
     # 后续所有 __init__(c1, c2, n=1, ...) 的复合模块
     conv_repeat_modules = frozenset({C3GhostExp, C3XExp, C3TRExp, HGBlockExp, C3SCConvExp,
-                                     C3FasterBlockExp, C2fMSBlockExp, C2fHGBlockExp, C3_Sonic, C3_HoGEdgeGate, C2f_GLFA, C2f_PFG})
+                                     C3FasterBlockExp, C2fMSBlockExp, C2fHGBlockExp, C3_Sonic, C3_HoGEdgeGate, C2f_GLFA, C2f_PFG, C2f_MSEF})
 
     # ✅ 变种SPP模块
     spp_modules = frozenset({SE_SPPF, SPPF_LSKA, DBSPPF, PMHSA, ASPP})  # 逐步加
