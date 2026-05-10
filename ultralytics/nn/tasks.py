f2acc1c5537e201460d7738da59a828929b2a6aa
Tsunade-hime <HinNyuuHime@outlook.com>
2026-05-10 17:26:08 +0800

    Add C2fRFCA to conv_modules and conv_repeat_modules

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -1676,11 +1676,11 @@ def parse_model(d, ch, verbose=True):
                               CAB, MSCAM, SCConv, C3GhostExp, C3XExp, C3TRExp,
                               HGBlockExp, C3SCConvExp, C3FasterBlockExp, C2fMSBlockExp, C2fHGBlockExp,
                               RFAConv, RFCAConv, RFCBAMConv, Sonic, C3_Sonic, C2f_GLFA, C2f_PFG, C2f_MSEF, C2f_Sonic,
-                              C3_PFG, GMR_Conv2d, C2fGMR})
+                              C3_PFG, GMR_Conv2d, C2fGMR, C2fRFCA})
     # 后续所有 __init__(c1, c2, n=1, ...) 的复合模块
     conv_repeat_modules = frozenset({C3GhostExp, C3XExp, C3TRExp, HGBlockExp, C3SCConvExp,
                                      C3FasterBlockExp, C2fMSBlockExp, C2fHGBlockExp, C3_Sonic, C2f_GLFA, C2f_PFG, C2f_MSEF, C2f_Sonic,
-                                     C3_PFG, C2fGMR})
+                                     C3_PFG, C2fGMR, C2fRFCA})
 
     # ✅ 变种SPP模块
     spp_modules = frozenset({SE_SPPF, SPPF_LSKA, DBSPPF, PMHSA, ASPP})  # 逐步加
